cba091a0d93afccd412e07568552742d0c933b25
Alex <olexandrpasalsky@gmail.com>
2022-06-05 18:56:32 +0300

    Analysis: funcs build graphs & correlation

---
+++ CryptoAnalysis/Analysis.py
@@ -0,0 +1,84 @@
+import matplotlib.pyplot as plt
+
+
+
+    #побудувати графік числових послідовностей значень масиву name_columns криптовалюти df(dataframe)
+def graph_timesequences(df, name_columns):
+    index = df.index[0]
+    title = 'Часова динаміка величин: '
+    for name in name_columns:
+        title = title + name + ', '
+    title = title + ' криптовалюти ' + df.loc[index]['Name']
+
+    fig, ax = plt.subplots(figsize=(15, 10))
+    for name in name_columns:
+        df[name].plot(ax=ax)
+    plt.title(title)
+    ax.grid()
+    ax.legend()
+    plt.show()
+
+
+    #впобудувати графік числових послідовностей значень масиву name_columns криптовалюти df(dataframe)
+        #за інтервал часу [start_date; end_date]
+def graph_interval_timesequences(df, name_columns, start_date, end_date):
+    index = df.index[0]
+    title = 'Часова динаміка величин: '
+    for name in name_columns:
+        title = title + name + ', '
+    title = title + ' криптовалюти ' + df.loc[index]['Name']+\
+            ' з ' + start_date + ' по ' + end_date
+
+    fig, ax = plt.subplots(figsize=(15, 10))
+    for name in name_columns:
+        df[[name]].loc[start_date + ' 00:00:00' : end_date + ' 00:00:00'].plot(ax=ax)
+    plt.title(title)
+    ax.grid()
+    ax.legend()
+    plt.show()
+
+
+    #побудувати графік числових послідовностей значення name_column криптовалют dfs(масив dataframes)
+def graph_timesequence_cryptos(dfs, name_column):
+    title = 'Часова динаміка величини ' + name_column + '\n криптовалют: '
+    for df in dfs:
+        title = title + df.loc[df.index[0]]['Name'] + ', '
+
+    fig, ax = plt.subplots(figsize=(15, 10))
+    linelegend = []
+    for df in dfs:
+        df[name_column].plot(ax=ax)
+        linelegend.append(df.loc[df.index[0]]['Name'])
+
+    plt.title(title)
+    ax.grid()
+    ax.legend(linelegend)
+    plt.show()
+
+
+    #побудувати графік числових послідовностей значення name_column криптовалют dfs(масив dataframes)
+    # за інтервал часу [start_date; end_date]
+def graph_interval_timesequence_cryptos(dfs, name_column, start_date, end_date):
+    title = 'Часова динаміка величини ' + name_column + ' з ' + start_date + ' по ' + end_date + '\n криптовалют: '
+    for df in dfs:
+        title = title + df.loc[df.index[0]]['Name'] + ', '
+
+    fig, ax = plt.subplots(figsize=(15, 10))
+    linelegend = []
+    for df in dfs:
+        df[name_column].loc[start_date + ' 00:00:00' : end_date + ' 00:00:00'].plot(ax=ax)
+        linelegend.append(df.loc[df.index[0]]['Name'])
+
+    plt.title(title)
+    ax.grid()
+    ax.legend(linelegend)
+    plt.show()
+
+
+
+
+    #матриця кореляції
+def correlation_matrix(df):
+    corr = df[['High', 'Low', 'Open', 'Close', 'Volume', 'Marketcap', 'amountToken']].corr()
+    print("Matrix correlation" + df.loc[df.index[0]]['Name'])
+    print(corr)
--- CryptoAnalysis/main.py
+++ CryptoAnalysis/main.py
@@ -4,6 +4,16 @@ from sqlalchemy import create_engine
 
 from Config import engine
 from ProcessData import *
+from Analysis import *
+
+
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', None)
+desired_width = 300
+pd.set_option('display.width', desired_width)
+
 
 
 if __name__ == '__main__':
@@ -14,3 +24,19 @@ if __name__ == '__main__':
     print(df.head(10).to_string())
     print(df.info())
 
+    '''
+    graph_timesequences(df, ['Open'])
+    graph_interval_timesequences(df, ['Open'], '2020-01-01', '2021-12-30')
+
+    graph_timesequences(df, ['Open', 'High'])
+    graph_interval_timesequences(df, ['Open', 'High'], '2020-01-01', '2021-12-30')
+
+    df2 = read_time_sequence("Litecoin")
+    create_column_amountToken(df2)
+    print(df2.head(10).to_string())
+    print(df2.info())
+    graph_timesequence_cryptos([df, df2], 'Open')
+    graph_interval_timesequence_cryptos([df, df2], 'Open', '2020-01-01', '2021-12-30')
+
+    correlation_matrix(df2)
+    '''
